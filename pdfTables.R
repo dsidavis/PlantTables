119acdfcc28ca92cedbc7527ab5c6f5bef98d70c
Duncan Temple Lang <duncan@r-project.org>
2016-08-03 08:48:11 -0700

    T44 wrong.  T01 T39

--- pdfTables.R
+++ pdfTables.R
@@ -27,6 +27,11 @@ function(file, show = TRUE)
     getColsFromBBox(bbox, "GRAND|MEAN", show)
 }
 
+getRank =
+    # Since we discarded the rank columns, here is a function to compute them.
+    # Smaller value means lower rank, i.e. c(3, 4, 2) gives a rank 2, 3, 1
+function(x)
+  match(x, sort(x))    
 
 get2004Filename =
 function(file)
